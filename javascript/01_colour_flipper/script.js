4af6839fd1034caac955d17d37a40c14567ed39d
Danish Shafi <danish.shafi@live.co.uk>
2023-05-23 00:45:45 +0100

    initial folder struc created

---
+++ javascript/01_colour_flipper/script.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomNum = getRandomNumber();
+  console.log(randomNum);
+
+  document.body.style.backgroundColor = colors[randomNum];
+  color.textContent = colors[randomNum];
+});
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+};
